c8326db5f7f52ff1f7e65434ae0b7e3f6231867e
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-15 12:25:55 -0700

    5

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { UserProps } from "../types/user"
+
+import Search from "../components/Search"
+
+import User from "../components/User"
+
+import { useState } from "react"
 
 const Home = () => {
+  const [user, setUser] = useState <UserProps | null> (null)
+  
+  const loadUser = async(userName: string) => {
+    const res = await fetch(`https:api.github.com/users/${userName}`)
+    const data = await res.json()
+    const {avatar_url, login, location, followers, following } = data
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    }
+    setUser(userData)
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <Search loadUser={loadUser}/>
+      <p>
+        {user && <User {...user} />}
+      </p>
+    </div>
   )
 }
 
